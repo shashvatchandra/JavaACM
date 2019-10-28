90e5e0c6c5fb25cdd4d907d443fae5b4b3a7a9d4
shashvatchandra <57087015+shashvatchandra@users.noreply.github.com>
2019-10-28 12:43:56 +0530

    Hacktoberfest 3

--- Program3.java
+++ Program3.java
@@ -1,8 +1,7 @@
-import java.io*;
 import java.util.*;
 class Program3
 {
-public static void main(String args[])throws IOException
+public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int n=0;
