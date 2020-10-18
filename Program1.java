8f87758422439092e3c2360dc4eb9ba8ad95d35a
shashvatchandra <57087015+shashvatchandra@users.noreply.github.com>
2020-10-19 00:19:16 +0530

    Ditto

--- Program1.java
+++ Program1.java
@@ -10,3 +10,15 @@ class Program1
 		System.out.println("Bruh the number you gave me was "+i);
 	}
 }
+import java.io.*;
+import java.util.*;
+class Program1
+{
+	public static void main(String args[])throws IOException
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Gimme a number");
+		int i=sc.nextInt();
+		System.out.println("Bruh the number you gave me was "+i);
+	}
+}
